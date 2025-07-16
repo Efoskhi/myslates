fe10386f85020bb1d149b4a6c1b89200e135c09a
ohanronnie <titiloyepaul68@gmail.com>
2025-07-16 14:34:04 +0100

    feat: termly result hook

---
+++ src/Hooks/useTermlyResult.ts
@@ -0,0 +1,341 @@
+import React from "react";
+import {
+  addFirebaseData,
+  getFirebaseData,
+  updateFirebaseData,
+} from "../utils/firebase";
+import { toast } from "react-hot-toast";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import { useAppContext } from "../context/AppContext";
+import useSubject from "./useSubject";
+import { get } from "http";
+import { all } from "axios";
+
+let fetchedStudents = [];
+
+function getGrade(total: number): string {
+  if (total >= 70) return "A";
+  if (total >= 60 && total <= 69) return "B";
+  if (total >= 50 && total <= 59) return "C";
+  if (total >= 46 && total <= 49) return "D";
+  if (total >= 41 && total <= 45) return "E";
+  return "F";
+}
+
+const useResultManagement = ({
+  shouldGetStudents = true,
+  shouldGetPrefetchedStudents = false,
+}) => {
+  const [students, setStudents] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(false);
+  const [pagination, setPagination] = React.useState();
+  const [classes, setClasses] = React.useState<any[]>([]);
+  const [allStudents, setAllStudents] = React.useState([]);
+
+  const [filters, setFilters] = React.useState({
+    page: 1,
+    pageSize: 100,
+  });
+  const [isSaving, setSaving] = React.useState(false);
+  const [studentResults, setStudentResults] = React.useState([
+    { id: 1, student: "", ca1: "", ca2: "", exam: "", remarks: "" },
+  ]);
+
+  const { currentSubject, user } = useAppContext();
+
+  const { subjectStudents, isLoadingSubjectStudents } = useSubject({
+    shouldGetSubjectStudents: true,
+  });
+
+  const addMoreStudentResult = () => {
+    setStudentResults([
+      ...studentResults,
+      {
+        id: studentResults.length + 1,
+        student: "",
+        ca1: "",
+        ca2: "",
+        exam: "",
+        remarks: "",
+      },
+    ]);
+  };
+  const handleStudentResultInputChange = (id, field, value) => {
+    const isDuplicate = studentResults.some(
+      (student) => student.student === value
+    );
+
+    if (isDuplicate) {
+      return toast.error("Duplicate student identified");
+    }
+
+    setStudentResults(
+      studentResults.map((student) =>
+        student.id === id ? { ...student, [field]: value } : student
+      )
+    );
+  };
+
+  const getSubjectStudents = async () => {
+    setLoading(isLoadingSubjectStudents);
+    setStudents(subjectStudents);
+  };
+
+  const getStudents = async (student_class, fetchAll) => {
+    if (!fetchAll && !student_class) return;
+
+    let query = [
+      ["school_id", "==", user.school_id],
+      ["role", "==", "learner"],
+      ...(fetchAll
+        ? [["student_class", "!=", student_class]]
+        : [["student_class", "==", student_class]]),
+    ];
+    const studentsSnapshot = await getFirebaseData({
+      collection: "users",
+      query: query as any,
+      pageSize: Infinity,
+    });
+
+    const _students = studentsSnapshot.data?.users || [];
+    setAllStudents(_students);
+    return _students;
+  };
+  const getClassesAndCategories = async () => {
+    const classesSnapshot = await getDocs(
+      query(collection(db, "Classes"), where("category", "==", "Nigerian"))
+    );
+    const _classes: any[] = [];
+    classesSnapshot.forEach((doc) => {
+      _classes.push({ id: doc.id, ...doc.data() });
+    });
+    setClasses(_classes);
+  };
+  const getPrefetchedStudents = async () => {
+    if (fetchedStudents.length > 0) {
+      return setStudents(fetchedStudents);
+    }
+
+    await getSubjectStudents();
+  };
+  const handleAddStudentResults = async ({
+    ca1,
+    ca2,
+    headTeacherComment,
+    term,
+    subjectName,
+    exam,
+    remark,
+    classTeacherComment,
+    className,
+    selectedSession,
+    studentId,
+    studentLength,
+  }) => {
+    const studentRef = doc(db, "users", studentId);
+    const studentPrevious = await getFirebaseData({
+      query: [["student_ref", "==", studentRef]],
+      collection: "StudentResults",
+      subcollections: ["ResultSubjects"],
+    });
+    const previousResult = studentPrevious.data?.StudentResults || [];
+    const previousSubjects = previousResult.map((result) => {
+      return (result.ResultSubjects || []).map((subject) => ({
+        ...subject,
+      }));
+    });
+
+    console.log("previousSubjects", previousSubjects);
+    const studentData = {
+      class_teacher_comment: classTeacherComment,
+      class_teacher_name: user.display_name,
+      head_teacher_comment: headTeacherComment,
+      is_approved: false,
+      number_in_class: studentLength,
+      result_status: "Pending",
+      school_id: user.school_id,
+      student_class: className,
+      session: selectedSession,
+      next_term_begin: "",
+      term_ending: "",
+      student_ref: studentRef,
+      term,
+    };
+    const subjectData = {
+      exam,
+      first_ca: ca1,
+      second_ca: ca2,
+      remark,
+      grade: getGrade(Number(ca1) + Number(ca2) + Number(exam)),
+      total: Number(ca1) + Number(ca2) + Number(exam),
+      subject_name: subjectName,
+    };
+  };
+  const handleAddResults = async () => {
+    try {
+      setSaving(true);
+
+      for (const item of studentResults) {
+        const { ca1, ca2, exam, remarks, student } = item;
+        if (!ca1 || !ca2 || !exam || !remarks || !student) {
+          toast.error("Please fill in all fields before submitting.");
+          setSaving(false);
+          return;
+        }
+      }
+      const results = studentResults.map((item) => {
+        const { ca1, ca2, exam, remarks, student } = item;
+        const studentDetails = JSON.parse(student);
+
+        return {
+          cleanData: {
+            class_teacher_comment: "",
+            class_teacher_name: user.display_name,
+            is_approved: false,
+            number_in_class: students.length,
+            result_status: "Pending",
+            school_id: user.school_id,
+            totalScore: ca1 + ca2 + exam,
+            student_class: studentDetails.student_class,
+            student_ref: {
+              isRef: true,
+              collection: "users",
+              id: studentDetails.uid,
+            },
+          },
+          rawData: item,
+          studentDetails,
+        };
+      });
+
+      const response = await Promise.all(
+        results.map(async (data) => {
+          let subCollectionData;
+          let subjectData = data.cleanData;
+
+          const { ca1, ca2, exam, remarks } = data.rawData;
+          const total = ca1 + ca2 + exam;
+          const grade = getGrade(Number(total));
+
+          subCollectionData = {
+            ResultSubjects: {
+              exam,
+              first_ca: ca1,
+              second_ca: ca2,
+              remark: remarks,
+              grade,
+              total,
+              subject_name: currentSubject.title,
+            },
+          };
+
+          const studentRef = doc(db, "users", data.studentDetails.uid);
+
+          let studentResponse = await getFirebaseData({
+            collection: "StudentResults",
+            query: [["student_ref", "==", studentRef]],
+            subcollections: ["ResultSubjects"],
+          });
+
+          const studentResult = studentResponse.data?.StudentResults;
+          const id = studentResult[0]?.id;
+          const totalResults = studentResult.length;
+
+          // console.log("totalResults", grandTotal)
+
+          // return;
+
+          if (totalResults) {
+            let totalFirstCA = 0;
+            let totalSecondCA = 0;
+            let totalExam = 0;
+
+            studentResult.forEach((entry) => {
+              entry.ResultSubjects?.forEach((subject) => {
+                totalFirstCA += subject.first_ca || 0;
+                totalSecondCA += subject.second_ca || 0;
+                totalExam += subject.exam || 0;
+              });
+            });
+
+            const grandTotal =
+              totalFirstCA +
+              totalSecondCA +
+              totalExam +
+              subCollectionData.ResultSubjects.total; // add total of the result to be added
+
+            await updateFirebaseData({
+              collection: "StudentResults",
+              data: {
+                total_score: grandTotal,
+              },
+              id,
+            });
+
+            return await addFirebaseData({
+              collection: "StudentResults",
+              successMessage: "",
+              subCollectionData,
+              id,
+            });
+          }
+
+          return await addFirebaseData({
+            collection: "StudentResults",
+            data: subjectData,
+            successMessage: "",
+            subCollectionData,
+          });
+        })
+      );
+
+      if (response[0].status === "error")
+        throw new Error("Something went wrong uploading result");
+
+      toast.success("Results have been uploaded");
+
+      setStudentResults([
+        { id: 1, student: "", ca1: "", ca2: "", exam: "", remarks: "" },
+      ]);
+    } catch (error) {
+      console.log("error", studentResults);
+      toast.error(error.message);
+    } finally {
+      setSaving(false);
+    }
+  };
+
+  React.useEffect(() => {
+    if (shouldGetPrefetchedStudents) getPrefetchedStudents();
+  }, []);
+
+  React.useEffect(() => {
+    if (shouldGetStudents) getSubjectStudents();
+  }, [isLoadingSubjectStudents]);
+
+  return {
+    isLoading,
+    isSaving,
+    students,
+    pagination,
+    subject: currentSubject,
+    studentResults,
+    handleAddResults,
+    addMoreStudentResult,
+    handleStudentResultInputChange,
+    classes,
+    getStudents,
+    allStudents,
+    getClassesAndCategories,
+  };
+};
+
+export default useResultManagement;
